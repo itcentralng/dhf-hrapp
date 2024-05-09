323123b6355bb96a2f8e1b8b31e30906659c840e
ishaaaq <141967886+ishaaaq@users.noreply.github.com>
2024-05-09 01:47:54 +0100

    updated create document page

---
+++ src/data/templatesData.js
@@ -0,0 +1,8 @@
+const templatesData = [
+  { name: "Study Leave Proposal", lastEditedDate: "July 14, 2023" },
+  { name: "Teaching Observation", lastEditedDate: "July 14, 2023" },
+  { name: "Permission Proposal", lastEditedDate: "July 14, 2023" },
+  { name: "Leave Of Absense", lastEditedDate: "July 14, 2023" },
+];
+
+export default templatesData;
--- src/pages/documents/CreateDocument.jsx
+++ src/pages/documents/CreateDocument.jsx
@@ -1,8 +1,10 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Paper, Stack, Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import AddIcon from "@mui/icons-material/Add";
-
+import templatesData from "../../data/templatesData";
+import templateImage from "../../assets/templateImage.svg";
+import templateIcon from "../../assets/templateIcon.svg";
 const CreateDocument = () => {
   const navigate = useNavigate();
 
@@ -16,39 +18,100 @@ const CreateDocument = () => {
           fontWeight: 500,
           fontSize: "20px",
           fontFamily: "urbanist",
+          mb: "13px",
         }}
       >
         Draft New Document
       </Typography>
-      <Box
-        sx={{
-          width: "177px",
-          height: "251px",
-          borderRadius: "4px",
-          border: "#D7D9DC 0.5px solid",
-          bgcolor: "white",
-          display: "flex",
-          flexDirection: "column",
-          gap: "34px",
-          alignItems: "center",
-          justifyContent: "center",
-          cursor: "pointer",
-        }}
-        onClick={handleNavigation}
-      >
-        <AddIcon
-          sx={{ color: "primary.main", width: "45px", height: "45px" }}
-        />
-        <Typography
+      <Stack direction="row" gap={1} sx={{}}>
+        <Box
           sx={{
-            fontWeight: 400,
-            fontSize: "14px",
-            fontFamily: "inter",
+            width: "187px",
+            mt: "-5px",
+            height: "255px",
+            borderRadius: "4px",
+            border: "#D7D9DC 0.5px solid",
+            bgcolor: "white",
+            display: "flex",
+            flexDirection: "column",
+            gap: "34px",
+            alignItems: "center",
+            justifyContent: "center",
+            cursor: "pointer",
           }}
+          onClick={handleNavigation}
         >
-          Blank Document
-        </Typography>
-      </Box>
+          <AddIcon
+            sx={{ color: "primary.main", width: "45px", height: "45px" }}
+          />
+          <Typography
+            sx={{
+              fontWeight: 400,
+              fontSize: "14px",
+              fontFamily: "inter",
+            }}
+          >
+            Blank Document
+          </Typography>
+        </Box>
+        {templatesData.map((template) => (
+          <Box
+            key={template.name}
+            sx={{
+              width: "200px",
+              height: "265px",
+              borderRadius: "4px",
+              backgroundImage: `url("${templateImage}")`,
+              backgroundSize: "cover",
+              mt: "-10px",
+              mb: "50px",
+              position: "relative",
+            }}
+          >
+            <Stack
+              direction="row"
+              gap={"4px"}
+              sx={{
+                bgcolor: "white",
+                marginTop: "100%",
+                mx: "auto",
+                borderRadius: "4px",
+                position: "absolute",
+                width: "162px",
+                height: "40px",
+                bottom: "3%",
+                left: "5%",
+                pl: "13px",
+                pt: "20px",
+              }}
+            >
+              <img
+                src={templateIcon}
+                style={{ width: "20px", height: "20px" }}
+              />
+              <Stack direction="column">
+                <Typography
+                  sx={{
+                    fontFamily: "urbanist",
+                    fontWeight: 400,
+                    fontSize: "12.8px",
+                  }}
+                >
+                  {template.name}
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "#5A626C",
+                    fontFamily: "urbanist",
+                    fontWeight: 300,
+                    fontSize: "8.5px",
+                  }}
+                >{`Last edited ${template.lastEditedDate}`}</Typography>
+              </Stack>
+            </Stack>
+          </Box>
+        ))}
+      </Stack>
     </Container>
   );
 };
