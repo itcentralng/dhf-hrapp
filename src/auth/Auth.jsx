1f6b1cf3bd77dfdfac70672fea8e73d07a2448d4
Mubarak <mubee2004@gmail.com>
2024-05-12 23:04:59 +0100

    removed userdetails console log

--- src/auth/Auth.jsx
+++ src/auth/Auth.jsx
@@ -4,7 +4,7 @@ import { Navigate } from "react-router-dom";
 
 const Auth = ({ children }) => {
   const user = useSelector((state) => state.user.user);
-  console.log(user);
+  // console.log(user);
   if (user === null) {
     return <Navigate to="/login" />;
   }
